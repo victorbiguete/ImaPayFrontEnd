99e57d5b5109c91d966107d1e2f543a42677b4a1
nilwandjg <nilwandjg@yahoo.com.br>
2024-02-17 19:01:31 -0300

    criação do componente de 'transacoes' e modificações genéricas  do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TransacoesComponent } from './componentes/transacoes/transacoes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TransacoesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
