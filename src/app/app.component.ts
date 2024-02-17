14c648ee8cff09264109068d7dd8929eafd88e5a
DandaraF <dandara-chr@hotmail.com>
2024-02-17 13:08:10 -0300

    criação do componente ação rapida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CartaoCreditoComponent } from './components/cartao-credito/cartao-credito.component';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, CartaoCreditoComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CartaoCreditoComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'ImaPayFrontEnd';
