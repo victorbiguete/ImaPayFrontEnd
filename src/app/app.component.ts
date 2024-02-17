f5363374d4aae22ad102fa4ee3f4dd363076e8d2
xdsribeiro@gmail.com <xdsribeiro@gmail.com>
2024-02-17 05:52:07 -0300

    Sera que meu commit vai funcionar corretamente?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PerfilComponent } from "./perfil/perfil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PerfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
