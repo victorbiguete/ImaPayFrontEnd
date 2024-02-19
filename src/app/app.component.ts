e1b7f00ac6d525317d2987dc7e7705bc50b36651
Alexandre de Souza Ribeiro <35141274+alexandredsr@users.noreply.github.com>
2024-02-19 12:32:13 -0300

    Add files via upload
    Atualizando os arquivos globais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from "./Componentes/body/body.component";
+import { NavbarComponent } from "./Componentes/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BodyComponent, NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ImaPayFrontEnd';
+  title = 'perfil';
 }
