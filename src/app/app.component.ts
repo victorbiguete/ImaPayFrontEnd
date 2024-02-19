368687812c5419ccac0d0b550f468d8c8d3a9adb
DandaraF <dandara-chr@hotmail.com>
2024-02-19 19:28:50 -0300

    remoção de imports não usados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CartaoCreditoComponent } from './componentes/cartao-credito/cartao-credito.component';
 import { HomeComponent } from "./componentes/home/home.component";
-import { CardComponent } from './componentes/card/card.component';
-import { GraficoDonutComponent } from './componentes/grafico-donut/grafico-donut.component';
-import { BarraProgressoComponent } from './componentes/barra-progresso/barra-progresso.component';
 import { TransacoesComponent } from './componentes/transacoes/transacoes.component';
 import { PerfilComponent } from "./perfil/perfil.component";
-import { TabelaComponent } from './componentes/tabela/tabela.component';
 
 @Component({
     selector: 'app-root',
@@ -17,14 +12,9 @@ import { TabelaComponent } from './componentes/tabela/tabela.component';
     styleUrl: './app.component.css',
     imports: [CommonModule,
               RouterOutlet,
-              CartaoCreditoComponent,
               HomeComponent,
-              CardComponent,
-              GraficoDonutComponent,
-              BarraProgressoComponent,
               TransacoesComponent,
-              PerfilComponent,
-              TabelaComponent
+              PerfilComponent
             ]
 })
 export class AppComponent {
