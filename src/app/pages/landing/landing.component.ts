f10633b4d1e47043c9992a8f49465a39c6918a72
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-15 13:07:29 -0300

    feat smartBanking presentation add to landingPage

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { HttpCpfService } from '../../services/httpCpf/http-cpf.service';
 import { CommonModule } from '@angular/common';
 import { NavbarLandingComponent } from '../../componentes/navbar-landing/navbar-landing.component';
 import { MainPresentationComponent } from '../../componentes/main-presentation/main-presentation.component';
+import { SmartBankingPresentationComponent } from '../../componentes/smart-banking-presentation/smart-banking-presentation.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, NavbarLandingComponent, MainPresentationComponent],
+  imports: [
+    CommonModule,
+    NavbarLandingComponent,
+    MainPresentationComponent,
+    SmartBankingPresentationComponent,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
