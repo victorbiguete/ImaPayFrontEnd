53276e0b978c05d67d198614cfda1d86d8af358d
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-14 23:20:54 -0300

    feat add main presentation for landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HttpCpfService } from '../../services/httpCpf/http-cpf.service';
 import { CommonModule } from '@angular/common';
 import { NavbarLandingComponent } from '../../componentes/navbar-landing/navbar-landing.component';
+import { MainPresentationComponent } from '../../componentes/main-presentation/main-presentation.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, NavbarLandingComponent],
+  imports: [CommonModule, NavbarLandingComponent, MainPresentationComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
