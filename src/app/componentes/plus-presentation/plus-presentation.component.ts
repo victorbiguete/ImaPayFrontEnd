05bf9822f61accaeacdbe1e7f518d02d71b5916f
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-15 15:24:50 -0300

    feat plus presentation

---
+++ src/app/componentes/plus-presentation/plus-presentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plus-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './plus-presentation.component.html',
+  styleUrl: './plus-presentation.component.css'
+})
+export class PlusPresentationComponent {
+
+}
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -5,6 +5,7 @@ import { MainPresentationComponent } from '../../componentes/main-presentation/m
 import { SmartBankingPresentationComponent } from '../../componentes/smart-banking-presentation/smart-banking-presentation.component';
 import { ProductsPresentationComponent } from '../../componentes/products-presentation/products-presentation.component';
 import { AnywhereAccPresentationComponent } from '../../componentes/anywhere-acc-presentation/anywhere-acc-presentation.component';
+import { PlusPresentationComponent } from '../../componentes/plus-presentation/plus-presentation.component';
 
 @Component({
   selector: 'app-landing',
@@ -16,6 +17,7 @@ import { AnywhereAccPresentationComponent } from '../../componentes/anywhere-acc
     SmartBankingPresentationComponent,
     ProductsPresentationComponent,
     AnywhereAccPresentationComponent,
+    PlusPresentationComponent,
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
