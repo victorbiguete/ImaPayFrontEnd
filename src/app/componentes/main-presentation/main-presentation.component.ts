cd1afe0f9a306bd60d89a42ab43f523648f6991b
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-14 22:21:07 -0300

    feat nav bar landing add

---
+++ src/app/componentes/main-presentation/main-presentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-presentation.component.html',
+  styleUrl: './main-presentation.component.css'
+})
+export class MainPresentationComponent {
+
+}
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpCpfService } from '../../services/httpCpf/http-cpf.service';
 import { CommonModule } from '@angular/common';
+import { NavbarLandingComponent } from '../../componentes/navbar-landing/navbar-landing.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarLandingComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
