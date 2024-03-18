86be8854bc4acdef09985bd2b5d020d9f1e0f851
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-18 09:57:06 -0300

    feat add localStorage on transacoes Page

--- src/app/pages/transacoes/transacoes.component.ts
+++ src/app/pages/transacoes/transacoes.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../../componentes/menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../../componentes/footer/footer.component';
-
+import { LoginService } from '../../services/login/login.service';
 
 interface Transacao {
-	tipo: string;
-	data: string;
-	nome: string;
-	valor: string;
+  tipo: string;
+  data: string;
+  nome: string;
+  valor: string;
   descricao: string;
 }
 @Component({
@@ -17,109 +17,108 @@ interface Transacao {
   standalone: true,
   imports: [CommonModule, MenuComponent, FormsModule, FooterComponent],
   templateUrl: './transacoes.component.html',
-  styleUrl: './transacoes.component.css'
+  styleUrl: './transacoes.component.css',
 })
-
 export class TransacoesComponent {
-
   transacoes = [
     //Dados apenas para teste:
     {
-      "tipo": "Transferência enviada",
-      "data": "23/02/2024",
-      "nome": "Stronger Sports",
-      "valor": "150,00",
-      "descricao": "Pix"
+      tipo: 'Transferência enviada',
+      data: '23/02/2024',
+      nome: 'Stronger Sports',
+      valor: '150,00',
+      descricao: 'Pix',
     },
     {
-      "tipo": "Transferência recebida",
-      "data": "23/02/2024",
-      "nome": "Ana Mariana Silva",
-      "valor": "600,00",
-      "descricao": "Pix"
+      tipo: 'Transferência recebida',
+      data: '23/02/2024',
+      nome: 'Ana Mariana Silva',
+      valor: '600,00',
+      descricao: 'Pix',
     },
     {
-      "tipo": "Pag. débito",
-      "data": "23/02/2024",
-      "nome": "Grupo Casas Bahia",
-      "valor": "450,00",
-      "descricao": ""
+      tipo: 'Pag. débito',
+      data: '23/02/2024',
+      nome: 'Grupo Casas Bahia',
+      valor: '450,00',
+      descricao: '',
     },
     {
-      "tipo": "Transferência enviada",
-      "data": "23/02/2024",
-      "nome": "João da Silva",
-      "valor": "200,00",
-      "descricao": "Pix"
+      tipo: 'Transferência enviada',
+      data: '23/02/2024',
+      nome: 'João da Silva',
+      valor: '200,00',
+      descricao: 'Pix',
     },
     {
-      "tipo": "Pag. débito",
-      "data": "23/02/2024",
-      "nome": "Ifood",
-      "valor": "250,00",
-      "descricao": ""
+      tipo: 'Pag. débito',
+      data: '23/02/2024',
+      nome: 'Ifood',
+      valor: '250,00',
+      descricao: '',
     },
     {
-      "tipo": "Pag. crédito",
-      "data": "23/02/2024",
-      "nome": "Auto Posto Cesar",
-      "valor": "50,00",
-      "descricao": ""
+      tipo: 'Pag. crédito',
+      data: '23/02/2024',
+      nome: 'Auto Posto Cesar',
+      valor: '50,00',
+      descricao: '',
     },
     {
-      "tipo": "Transferência enviada",
-      "data": "23/02/2024",
-      "nome": "Cleide da Silva",
-      "valor": "500,00",
-      "descricao": "Pix"
+      tipo: 'Transferência enviada',
+      data: '23/02/2024',
+      nome: 'Cleide da Silva',
+      valor: '500,00',
+      descricao: 'Pix',
     },
     {
-      "tipo": "Pag. débito",
-      "data": "22/02/2024",
-      "nome": "Ifood",
-      "valor": "150,00",
-      "descricao": ""
+      tipo: 'Pag. débito',
+      data: '22/02/2024',
+      nome: 'Ifood',
+      valor: '150,00',
+      descricao: '',
     },
     {
-      "tipo": "Pag. crédito",
-      "data": "22/02/2024",
-      "nome": "Strong Sports",
-      "valor": "350,00",
-      "descricao": ""
+      tipo: 'Pag. crédito',
+      data: '22/02/2024',
+      nome: 'Strong Sports',
+      valor: '350,00',
+      descricao: '',
     },
     {
-      "tipo": "Pag. débito",
-      "data": "21/02/2024",
-      "nome": "Ifood",
-      "valor": "150,00",
-      "descricao": ""
+      tipo: 'Pag. débito',
+      data: '21/02/2024',
+      nome: 'Ifood',
+      valor: '150,00',
+      descricao: '',
     },
     {
-      "tipo": "Remuneração/salário",
-      "data": "21/02/2024",
-      "nome": "Amazon Services",
-      "valor": "15.000,00",
-      "descricao": ""
-    },
-
-    {
-      "tipo": "Pag. crédito",
-      "data": "20/02/2024",
-      "nome": "Coco Bambu",
-      "valor": "450,00",
-      "descricao": ""
+      tipo: 'Remuneração/salário',
+      data: '21/02/2024',
+      nome: 'Amazon Services',
+      valor: '15.000,00',
+      descricao: '',
     },
 
     {
-      "tipo": "Pag. débito",
-      "data": "19/02/2024",
-      "nome": "Ifood",
-      "valor": "97,45",
-      "descricao": ""
-    }
-  ]
+      tipo: 'Pag. crédito',
+      data: '20/02/2024',
+      nome: 'Coco Bambu',
+      valor: '450,00',
+      descricao: '',
+    },
+
+    {
+      tipo: 'Pag. débito',
+      data: '19/02/2024',
+      nome: 'Ifood',
+      valor: '97,45',
+      descricao: '',
+    },
+  ];
   imagemVisivel: boolean = true;
-  saldo: string = "*****";
+  // saldo: string = '*****';
+  saldo?: number;
   valorVisivel: boolean = false;
 
   transacoesOriginal: Transacao[] = [...this.transacoes];
@@ -127,22 +126,28 @@ export class TransacoesComponent {
   filtroTipo: keyof Transacao = 'tipo';
   filtroValor: any = '';
 
+  constructor(private _loginService: LoginService) {
+    this.imagemVisivel = true;
+    this.saldo = this._loginService.loggedUser?.amount ?? 102.64;
+  }
 
   trocarImagem() {
     this.imagemVisivel = !this.imagemVisivel;
-    if (this.imagemVisivel) {
-      this.saldo = "*****";
-    } else {
-      this.saldo = "102,64";
-    }
+    // if (this.imagemVisivel) {
+    //    this.saldo = "*****";
+    // } else {
+    //    this.saldo =  '102,64';
+    // }
   }
 
   aplicarFiltros(): void {
-    console.log("filtroTipo=" +this.filtroTipo)
+    console.log('filtroTipo=' + this.filtroTipo);
     if (this.filtroValor !== '') {
-
-      this.transacoes = this.transacoesOriginal.filter(transacao => {
-        return transacao[this.filtroTipo].toString().toLowerCase().includes(this.filtroValor.toString().toLowerCase());
+      this.transacoes = this.transacoesOriginal.filter((transacao) => {
+        return transacao[this.filtroTipo]
+          .toString()
+          .toLowerCase()
+          .includes(this.filtroValor.toString().toLowerCase());
       });
     } else {
       this.transacoes = this.transacoesOriginal;
