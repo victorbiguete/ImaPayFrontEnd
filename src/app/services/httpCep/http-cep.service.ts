e4634bbdd921b1f0b84b129547266a4dbf244a57
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-23 11:19:35 -0300

    fix typo on httpCep , was httpCpf

--- src/app/services/httpCep/http-cep.service.ts
+++ src/app/services/httpCep/http-cep.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class HttpCpfService {
+export class HttpCepService {
   constructor(private http: HttpClient) {}
 
   private url = 'https://brasilapi.com.br/api/cep/v1/';
